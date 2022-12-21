8617b2ed295ba3db786c8b3d1e906247205822bd
gary5413 <5413gary@gmail.com>
2022-12-21 09:45:19 +0800

    init SpringBoot Project

---
+++ src/main/java/com/garylee/springbootlabshoppingmall/SpringbootLabShoppingmallApplication.java
@@ -0,0 +1,13 @@
+package com.garylee.springbootlabshoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLabShoppingmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLabShoppingmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/garylee/springbootlabshoppingmall/SpringbootLabShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garylee.springbootlabshoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLabShoppingmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

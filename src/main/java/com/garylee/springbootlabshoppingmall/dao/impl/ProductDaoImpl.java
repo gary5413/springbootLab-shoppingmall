664335670a6bcf89f71791ac3e70358cd14d25a0
gary5413 <5413gary@gmail.com>
2022-12-23 16:37:56 +0800

    商品功能-實作商品列表(提煉程式)

--- src/main/java/com/garylee/springbootlabshoppingmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/garylee/springbootlabshoppingmall/dao/impl/ProductDaoImpl.java
@@ -40,19 +40,21 @@ public class ProductDaoImpl implements ProductDao {
         String sql="SELECT product_id,product_name, category, image_url, price, stock, description, created_date," +
                 " last_modified_date FROM product WHERE 1=1";
         HashMap<String, Object> map = new HashMap<>();
-//        查詢條件
+//       查詢條件
+//       提煉程式
+        sql=addFilteringSql(sql,map,productQueryParams);
 //        if(category !=null){
-        if(productQueryParams.getCategory() !=null){
-            sql = sql+" AND category = :category";
-//            map.put("category",category.name());
-            map.put("category",productQueryParams.getCategory().name());
-        }
-//        if(search !=null){
-        if(productQueryParams.getSearch() !=null){
-            sql =sql +" AND product_name LIKE :search";
-//            map.put("search","%"+search+"%");
-            map.put("search","%"+productQueryParams.getSearch()+"%");
-        }
+//        if(productQueryParams.getCategory() !=null){
+//            sql = sql+" AND category = :category";
+////            map.put("category",category.name());
+//            map.put("category",productQueryParams.getCategory().name());
+//        }
+////        if(search !=null){
+//        if(productQueryParams.getSearch() !=null){
+//            sql =sql +" AND product_name LIKE :search";
+////            map.put("search","%"+search+"%");
+//            map.put("search","%"+productQueryParams.getSearch()+"%");
+//        }
 //        排序
         sql =sql + " ORDER BY "+productQueryParams.getOrderBy() +" "+productQueryParams.getSort();
 //        分頁
@@ -67,18 +69,21 @@ public class ProductDaoImpl implements ProductDao {
     public Integer countProduct(ProductQueryParams productQueryParams) {
         String sql="SELECT count(*) FROM product WHERE 1=1";
         HashMap<String, Object> map = new HashMap<>();
-        //        查詢條件
-        if(productQueryParams.getCategory() !=null){
-            sql = sql+" AND category = :category";
-            map.put("category",productQueryParams.getCategory().name());
-        }
-        if(productQueryParams.getSearch() !=null){
-            sql =sql +" AND product_name LIKE :search";
-            map.put("search","%"+productQueryParams.getSearch()+"%");
-        }
+//        查詢條件
+//        提煉程式
+        sql=addFilteringSql(sql,map,productQueryParams);
+//        if(productQueryParams.getCategory() !=null){
+//            sql = sql+" AND category = :category";
+//            map.put("category",productQueryParams.getCategory().name());
+//        }
+//        if(productQueryParams.getSearch() !=null){
+//            sql =sql +" AND product_name LIKE :search";
+//            map.put("search","%"+productQueryParams.getSearch()+"%");
+//        }
         Integer total = namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
         return total;
     }
+//    最一開始的寫法
 //    @Override
 //    public List<Product> getProducts() {
 //        String sql="SELECT product_id,product_name, category, image_url, price, stock, description, created_date," +
@@ -150,5 +155,17 @@ public class ProductDaoImpl implements ProductDao {
         HashMap<String, Object> map = new HashMap<>();
         map.put("productId",productId);
         namedParameterJdbcTemplate.update(sql,map);
+    }
+    private String addFilteringSql(String sql,Map<String,Object> map,ProductQueryParams productQueryParams ){
+        if(productQueryParams.getCategory() !=null){
+            sql = sql+" AND category = :category";
+            map.put("category",productQueryParams.getCategory().name());
+        }
+        if(productQueryParams.getSearch() !=null){
+            sql =sql +" AND product_name LIKE :search";
+            map.put("search","%"+productQueryParams.getSearch()+"%");
+        }
+        Integer total = namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
+        return sql;
     }
 }
